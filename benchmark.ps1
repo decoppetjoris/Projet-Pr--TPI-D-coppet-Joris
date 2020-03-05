9e5f72a39924b7b4d8e03450cddbb9117b093d30
joris <joris.decoppet@cpnv.ch>
2020-03-05 10:37:42 +0100

    Get data finie

--- benchmark.ps1
+++ benchmark.ps1
@@ -34,7 +34,7 @@ function GetData {
         WriteLog "Erreur, la date n a pas ete generee correctement"
         Exit
     }
-
+    $Data = $DataDate
     #Heure
     try{
         $DataHeure = Get-Date -UFormat "%T"
@@ -42,7 +42,7 @@ function GetData {
         WriteLog "Erreur, l heure n a pas ete generee correctement"
         Exit
     }
-
+    $Data += ";" + $DataHeure
     #Nom
     try{
         $DataNom = $env:COMPUTERNAME
@@ -50,7 +50,7 @@ function GetData {
         WriteLog "Erreur, le nom de l'ordinateur n a pas ete generee correctement"
         Exit
     }
-    
+    $Data += ";" + $DataNom
     #Utilisateur connecté
     try{
         $DataUser = $env:UserName
@@ -58,7 +58,7 @@ function GetData {
         WriteLog "Erreur, le nom de l utilisateur connecte n a pas ete generee correctement"
         Exit
     }
-    
+    $Data += ";" + $DataUser
     #Modèle
     try{
         $DataModele = (Get-WmiObject -Class:Win32_ComputerSystem).Model
@@ -66,7 +66,7 @@ function GetData {
         WriteLog "Erreur, le model du pc n a pas pu etre recupere"
         Exit
     }
-    
+    $Data += ";" + $DataModele
     #SN
     try{
         $DataSn = (Get-WmiObject -Class:Win32_BIOS).SerialNumber
@@ -74,13 +74,11 @@ function GetData {
         WriteLog "Erreur, le sn du pc n a pas pu etre recupere"
         Exit
     }
-    
+    $Data += ";" + $DataSn
     #MAC
     try{
         $DataAllMac = get-wmiobject -class "Win32_NetworkAdapterConfiguration" |Where{$_.IpEnabled -Match "True"}  
-     
         foreach ($DataAllMacItem in $DataAllMac) {
-          
             if($DataAllMacItem.Description -notlike "*VMware*"){
                 $DataMac = $DataAllMacItem.MACAddress
             }
@@ -89,34 +87,95 @@ function GetData {
         WriteLog "Erreur, la Mac du pc n a pas pu etre recupere"
         Exit
     }
-    
+    $Data += ";" + $DataMac
     #IP
     try{
-        $info = Test-Connection -ComputerName (hostname) -count 1 | Select IPV4Address
-        $DataIp = $info.IPV4Address
+        $DataIp = $(Test-Connection -ComputerName (hostname) -Count 1).IPV4Address
     }catch{
         WriteLog "Erreur, l adresse IP du pc n a pas pu etre recupere"
         Exit
     }
-    
+    $Data += ";" + $DataIp
 
     # Calculer les datas
     #Depuis combien de temps le PC est allume
-
+    try{
+        $os = Get-WmiObject -Class win32_operatingsystem
+        $CalcTempsAlumPc = (get-date) - $os.ConvertToDateTime($os.LastBootUpTime)
+    }catch{
+        WriteLog "Erreur, impossible de savoir depuis combien de temps le PC est allume"
+        Exit
+    }
+    $Calc = $CalcTempsAlumPc.ToString()
     #Depuis combien de temps la session est ouverte
-
+    try{
+        $QuserData = quser
+        ForEach ($Quser in $QuserData){
+            if ($Quser.SubString(1, 20).Trim() -like $env:UserName){
+                $CalcTempsAlumSession = $Quser.SubString(65)
+            }
+        }
+    }catch{
+        WriteLog "Erreur, impossible de savoir depuis combien de temps la session est active"
+        Exit
+    }
+    $Calc += ";" + $CalcTempsAlumSession.ToString()
     #La latence de la passerelle
-
+    try{
+        $CalcLatPass = (Test-Connection -ComputerName google.ch -Count 1).ResponseTime
+    }catch{
+        WriteLog "Erreur, la latence de la passerelle n'a pas pu être calculee"
+        Exit
+    }
+    $Calc += ";" + $CalcLatPass.ToString()
     #Le temps d'ouverture d'un fichier Word
-
+    try{
+        $CalcTempWord  = Measure-Command -Expression {
+            $Word = New-Object -ComObject Word.Application
+            $Word.Documents.Open("C:\Projet-Pr--TPI-D-coppet-Joris\Fichier a ouvrir\fichier a ourvir par le script.doc",$True,$True)
+            $word.Ready
+        }
+        
+        #ferme le document word qui vient d etre ouvert
+        Get-Process winword | foreach { 
+            if ((($(get-date) - $_.StartTime).Seconds) -lt 10) {
+                Stop-Process $_.ID
+            }
+        }
+    }catch{
+        WriteLog "Erreur, le temps d ouverture du fichier word n'a pas pu être calcule"
+        Exit
+    }
+    $Calc += ";" + $CalcTempWord.ToString()
     #Le temps d'ouverture d'un fichier Excel
-
+    try{
+        $CalcTempExcel  = Measure-Command -Expression {
+            $excel = New-Object -ComObject excel.Application
+            $excel.Workbooks.Open("C:\Projet-Pr--TPI-D-coppet-Joris\Fichier a ouvrir\fichier a ouvrir par le script.xlsx",$True,$True)
+            $excel.Ready
+        }
+        
+        #ferme le document word qui vient d etre ouvert
+        Get-Process excel | foreach { 
+            if ((($(get-date) - $_.StartTime).Seconds) -lt 10) {
+                Stop-Process $_.ID
+            }
+        }
+    }catch{
+        WriteLog "Erreur, le temps d ouverture du fichier word n'a pas pu être calcule"
+        Exit
+    }
+    $Calc += ";" + $CalcTempExcel.ToString()
     #Le taux de transfert d'un fichier sur le LAN
+    $item = get-item 'C:\Projet-Pr--TPI-D-coppet-Joris\Fichier a ouvrir\text1.txt'
+    $time = Measure-Command -Expression {Copy-Item -literalpath 'C:\Projet-Pr--TPI-D-coppet-Joris\Fichier a ouvrir\text1.txt' 'X:\temps\text2.txt'} 
+    $CalcTauxTransfert = ($item.length/1024/1024) / $time.TotalSeconds
+    $Calc += ";" +  $CalcTauxTransfert.ToString()
 
     # Mettre toutes les datas dans une var
-    $Data = $DataDate + ";" + $DataHeure + ";" + $DataNom + ";" + $DataUser + ";" + $DataModele + ";" + $DataSn + ";" + $DataMac + ";" + $DataIp
+    $AllData = $Data + ";" + $Calc
 
-    return $Data
+    return $AllData
 }
 
 # fonction qui sert a ecrire dans le CSV
@@ -130,7 +189,7 @@ function WriteCSV {
         $Data | Export-Csv -Path $CsvPath -NoTypeInformation -Delimiter ";"
     }catch{
         WriteLog "Erreur, l ecriture dans le fichier CSV n a pas fonctionne"
-        Exite
+        Exit
     }
     
 }
@@ -150,7 +209,10 @@ $LogPath = "C:\Projet-Pr--TPI-D-coppet-Joris\Log\$(get-date -f yyyy.dd.MM.HH.mm)
 
 # Met en place le chemin pour le fichier CSV qui contient les données
 $CsvPath = "C:\Projet-Pr--TPI-D-coppet-Joris\Data\data.csv"
+if(!Test-Path($CsvPath)){
+    
+}
 
 $Info = GetData
 
-WriteLog $Info
+WriteCSV $Info
